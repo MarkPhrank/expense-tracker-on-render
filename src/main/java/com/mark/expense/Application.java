bcbc42149ac96b101675d2549ed58a40dbd86726
Mark <107129916+MarkPhrank@users.noreply.github.com>
2026-05-12 13:12:16 +0400

    Update Application.java

--- src/main/java/com/mark/expense/Application.java
+++ src/main/java/com/mark/expense/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        UserManager.initTable(); // 🔥 Создаёт таблицу users при старте
         SpringApplication.run(Application.class, args);
     }
 }
